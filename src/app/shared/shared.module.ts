bb7b3dcf96429f477bf1733dfba93695f0c298ac
unknown <eli91ai@gmail.com>
2020-03-23 12:37:42 +0100

    my-twimps and favorite-twimps components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { TwimpCardComponent } from './twimp/twimp-card/twimp-card.component';
 import { TwimpListComponent } from './twimp/twimp-list/twimp-list.component';
@@ -7,9 +8,10 @@ import { AuthorCardComponent } from './author/author-card/author-card.component'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [TwimpCardComponent, TwimpListComponent, AuthorCardComponent],
-  exports: [TwimpCardComponent, TwimpListComponent, AuthorCardComponent]
+  exports: [TwimpCardComponent, TwimpListComponent, AuthorCardComponent, RouterModule]
 })
 export class SharedModule { }
